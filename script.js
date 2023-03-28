6d659748095efd5e07fae19b456cca1646c46f1a
Rilnga <nuelagyei992@gmial.com>
2023-03-28 14:30:35 +0000

    Functions

--- script.js
+++ script.js
@@ -18,9 +18,10 @@ logger();
 
 function fruitProcessor(apples, oranges) {
     console.log(apples, oranges);
-    const juice = 'Juice with ${oranges} oranges.'
+    const juice = 'Juice with ${apples}apples and ${oranges} oranges.'
     return juice;
 }
 
 const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice)
+console.log(appleJuice);
+console.log(fruitProcessor(10, 2));
