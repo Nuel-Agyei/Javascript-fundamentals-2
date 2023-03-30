40be927f53f203927e8fef3b81827d95e7c31b82
Rilnga <nuelagyei992@gmial.com>
2023-03-30 09:22:29 +0000

    Function calling

--- script.js
+++ script.js
@@ -30,29 +30,43 @@
 // console.log(appleOrangeJuice);
 
 
-function calcAge1 (birthYear) {
-    const age = 2023 - birthYear;
-    return age;
+// function calcAge1 (birthYear) {
+//     const age = 2023 - birthYear;
+//     return age;
+// }
+//  const age1 = calcAge1(2004);
+
+//  const calcAge2 = function (birthYear) {
+//     return 2023 - birthYear;
+//  }
+//  const age2 = calcAge2(2003);
+//  console.log(age1, age2);
+
+
+
+//  //arrow function
+//  const  calcAge3 = birthYear => 2037 - birthYear;
+//  const age3 = calcAge3(2003)
+//  console.log(age3);
+
+//  const retirementYear = birthYear =>{
+//     const age = 2023 - birthYear;
+//     const retirement = 60 - age;
+//     return retirement;
+//  }
+
+//  console.log(retirementYear(2003));
+
+
+function cutFruitPieces(fruit) {
+   return fruit * 4;
 }
- const age1 = calcAge1(2004);
 
- const calcAge2 = function (birthYear) {
-    return 2023 - birthYear;
+function fruitProcessor(apples, oranges) {
+   const applePieces = cutFruitPieces(apples);
+   const orangePieces = cutFruitPieces(oranges);
+       console.log(apples, oranges);
+       const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+     return juice;
  }
- const age2 = calcAge2(2003);
- console.log(age1, age2);
-
-
-
- //arrow function
- const  calcAge3 = birthYear => 2037 - birthYear;
- const age3 = calcAge3(2003)
- console.log(age3);
-
- const retirementYear = birthYear =>{
-    const age = 2023 - birthYear;
-    const retirement = 60 - age;
-    return retirement;
- }
-
- console.log(retirementYear(2003));
+ console.log(fruitProcessor(2, 3));
